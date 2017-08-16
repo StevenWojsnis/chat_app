206631ee81aa2954d19b292c8f11c4ab9ba4a272
Steve Wojsnis <stevewojsnis@Steves-MBP.fios-router.home>
2017-08-16 01:46:43 -0400

    Added geolocation support via geolocation api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate the correct message object', () => {
@@ -11,5 +11,19 @@ describe('generateMessage', () => {
     expect(message.from).toBe(from);
     expect(message.text).toBe(text);
     expect(message.createdAt).toBeA('number');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    const from = 'Admin';
+    const latitude = 1;
+    const longitude = 2;
+
+    var location = generateLocationMessage(from, latitude, longitude);
+
+    expect(location.from).toBe('Admin');
+    expect(location.url).toBe('https://www.google.com/maps?q=1,2');
+    expect(location.createdAt).toBeA('number');
   });
 });
